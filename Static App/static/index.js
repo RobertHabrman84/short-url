1971360215ffcfca325b1bcf06710ca5d068e79d
RobertHabrman84 <72526872+RobertHabrman84@users.noreply.github.com>
2022-05-13 00:16:22 +0200

    Update index.js

--- Static App/static/index.js
+++ Static App/static/index.js
@@ -1,6 +1,6 @@
 //from parameters query full URL
 var shorturl = window.location.pathname.substr(1);
-var baseURL = "https://apiurlshort.azure-api.net/urlshortfunctionapp";
+var baseURL = "https://apiurlshort.azure-api.net/urlshortfunctionapp/GenerateShortURL";
 var requestURL = baseURL.concat(shorturl);
 
 var request = new XMLHttpRequest()
